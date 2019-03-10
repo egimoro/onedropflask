508d8f5161aff90f82feff4e08b891350f4d0eac
egimoro <gimoremma@gmail.com>
2019-03-10 14:39:51 +0100

    About Page

--- flaskdrop/flaskdrop.py
+++ flaskdrop/flaskdrop.py
@@ -23,7 +23,7 @@ def jambo():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 if __name__=='__main__': #if the name of the app matches the main script jambo
      app.run(debug=True)  #run the app when debugging
