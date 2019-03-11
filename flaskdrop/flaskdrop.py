ca91a3ee7b6e28d30d47c7ed3db3cb013b98f95d
egimoro <gimoremma@gmail.com>
2019-03-11 01:29:40 +0100

    register

--- flaskdrop/flaskdrop.py
+++ flaskdrop/flaskdrop.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template,url_for #render_template enables the html template
                                                 #url_for finds exact location of the url
+
+
+from forms import RegistrationForm,LoginForm
+
 app=Flask(__name__) #the name of the application package
+
+app.config['SECRET_KEY']='88fe5c0981ba01f5b81bcdb8c12dad29'
 
 posts=[
      {
@@ -25,6 +31,16 @@ def jambo():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+     form=RegistrationForm()
+     return render_template('register.html',title='Register',form=form)
+     
+@app.route("/login")
+def login():
+     form=LoginForm()
+     return render_template('login.html',title='Login',form=form)
 
 if __name__=='__main__': #if the name of the app matches the main script jambo
      app.run(debug=True)  #run the app when debugging
