31c3b52cf944b545ea87b26cf720878865ca4e93
egimoro <gimoremma@gmail.com>
2019-03-12 01:02:37 +0100

    log admin test

--- flaskdrop/flaskdrop.py
+++ flaskdrop/flaskdrop.py
@@ -45,10 +45,10 @@ def register():
 def login():
      form=LoginForm()
      if form.validate_on_submit():
-          if form.email.data=='admin@blog.com' and form.password.data=='password':
-               flash('You have been logged in!','success')
-               return redirect(url_for('home'))
-          else:
+           if form.email.data=='admin@blog.com' and form.password.data=='password':
+                flash('You have been logged in!','success')
+                return redirect(url_for('home'))
+           else:
                flash('Login Unsuccessful. Please check username and password','danger')
      return render_template('login.html',title='Login',form=form)
 
