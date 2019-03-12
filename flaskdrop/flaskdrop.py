fd7eea6bea77079dbdfb1ad72d3ff7a6006bf4ff
egimoro <gimoremma@gmail.com>
2019-03-12 23:49:16 +0100

    flash success

--- flaskdrop/flaskdrop.py
+++ flaskdrop/flaskdrop.py
@@ -43,15 +43,16 @@ def register():
      
 @app.route("/login",methods=['GET','POST'])
 def login():
-     form=LoginForm()
-     if form.validate_on_submit():
+      form=LoginForm()
+      if form.validate_on_submit():
            if form.email.data=='admin@blog.com' and form.password.data=='password':
                 flash('You have been logged in!','success')
                 return redirect(url_for('home'))
            else:
-               flash('Login Unsuccessful. Please check username and password','danger')
-     return render_template('login.html',title='Login',form=form)
+                flash('Login Unsuccessful. Please check username and password','danger')
+      return render_template('login.html',title='Login',form=form)
 
 if __name__=='__main__': #if the name of the app matches the main script jambo
      app.run(debug=True)  #run the app when debugging
+
 
--- flaskdrop/forms.py
+++ flaskdrop/forms.py
@@ -11,7 +11,6 @@ class RegistrationForm(FlaskForm):
     submit=SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email=StringField('Email',validators=[DataRequired(),Email()])
     password=PasswordField('Password',validators=[DataRequired()])
     remember=BooleanField('Remember Me')
