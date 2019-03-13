7e617f978d28402b27d9425c18037f6b009f55ee
egimoro <gimoremma@gmail.com>
2019-03-13 23:44:54 +0100

    login and logout

--- flaskdrop/flaskdrip/__init__.py
+++ flaskdrop/flaskdrip/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template,url_for,flash,redirect #render_template
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 
 app=Flask(__name__) #the name of the application package
@@ -12,5 +12,6 @@ app.config['SECRET_KEY']='88fe5c0981ba01f5b81bcdb8c12dad29'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db' #creates database in the pc location
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
 
 from flaskdrip import routes
--- flaskdrop/flaskdrip/routes.py
+++ flaskdrop/flaskdrip/routes.py
@@ -2,6 +2,7 @@ from flaskdrip.forms import RegistrationForm,LoginForm
 from flask import Flask, render_template,url_for,flash,redirect
 from flaskdrip import app,db,bcrypt
 from flaskdrip.models import User,Post 
+from flask_login import login_user,current_user,logout_user
  
 posts=[
      {
@@ -29,6 +30,8 @@ def about():
 
 @app.route("/register",methods=['GET','POST'])
 def register():
+     if current_user.is_authenticated:
+          return redirect(url_for('home'))
      form=RegistrationForm()
      if form.validate_on_submit():
          hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -42,12 +45,20 @@ def register():
      
 @app.route("/login",methods=['GET','POST'])
 def login():
-      form=LoginForm()
-      if form.validate_on_submit():
-           if form.email.data=='admin@blog.com' and form.password.data=='password':
-                flash('You have been logged in!','success')
+     if current_user.is_authenticated:
+          return redirect(url_for('home'))
+     form=LoginForm()
+     if form.validate_on_submit():
+           user=User.query.filter_by(email=form.email.data).first()
+           if user and bcrypt.check_password_hash(user.password,form.password.data):
+                login_user(user,remember=form.remember.data)
                 return redirect(url_for('home'))
            else:
-                flash('Login Unsuccessful. Please check username and password','danger')
-      return render_template('login.html',title='Login',form=form)
+                flash('Login Unsuccessful. Please check email and password','danger')
+     return render_template('login.html',title='Login',form=form)
 
+@app.route('/logout')
+def logout():
+     logout_user()
+     return redirect(url_for('home'))
+     
