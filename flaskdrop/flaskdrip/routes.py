947e47b7b4148b11f7f8ea216106ab5e8e22f5e3
egimoro <gimoremma@gmail.com>
2019-03-14 00:17:44 +0100

    account

--- flaskdrop/flaskdrip/routes.py
+++ flaskdrop/flaskdrip/routes.py
@@ -1,8 +1,8 @@
 from flaskdrip.forms import RegistrationForm,LoginForm
-from flask import Flask, render_template,url_for,flash,redirect
+from flask import Flask, render_template,url_for,flash,redirect,request
 from flaskdrip import app,db,bcrypt
 from flaskdrip.models import User,Post 
-from flask_login import login_user,current_user,logout_user
+from flask_login import login_user,current_user,logout_user,login_required
  
 posts=[
      {
@@ -52,7 +52,9 @@ def login():
            user=User.query.filter_by(email=form.email.data).first()
            if user and bcrypt.check_password_hash(user.password,form.password.data):
                 login_user(user,remember=form.remember.data)
-                return redirect(url_for('home'))
+                next_page=request.args.get('next')
+                return redirect(next_page) if next_page else redirect (url_for('home')) 
+                #ternary conditional redirect to next page if next page is available
            else:
                 flash('Login Unsuccessful. Please check email and password','danger')
      return render_template('login.html',title='Login',form=form)
@@ -61,4 +63,8 @@ def login():
 def logout():
      logout_user()
      return redirect(url_for('home'))
-     
+
+@app.route('/account')
+@login_required
+def account():
+     return render_template('account.html',title='Account')
