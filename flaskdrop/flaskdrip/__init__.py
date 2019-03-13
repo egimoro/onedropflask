f26d847223f8ea7bb6aed224eaf2631fffc93cfa
egimoro <gimoremma@gmail.com>
2019-03-13 16:48:07 +0100

    routes

---
+++ flaskdrop/flaskdrip/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,url_for,flash,redirect #render_template enables the html template
+                                                #url_for finds exact location of the url
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app=Flask(__name__) #the name of the application package
+
+app.config['SECRET_KEY']='88fe5c0981ba01f5b81bcdb8c12dad29'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db' #creates database in the pc location
+db=SQLAlchemy(app)
+
+from flaskdrip import routes
