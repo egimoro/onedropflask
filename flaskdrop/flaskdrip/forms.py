950b9122055b71f4c26591697356e1ab5217e6a1
egimoro <gimoremma@gmail.com>
2019-03-17 13:50:54 +0100

    update profile picture

--- flaskdrop/flaskdrip/forms.py
+++ flaskdrop/flaskdrip/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flaskdrip.models import User
 from flask_login import current_user
+from flask_wtf.file import FileField,FileAllowed
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
@@ -28,23 +29,20 @@ class LoginForm(FlaskForm):
     remember=BooleanField('Remember Me')
     submit=SubmitField('Login')
 
-class UpdateAccountForm(FlaskForm):
+class UpdateAccountForm(FlaskForm): #query the database to see if a value is taken 
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email=StringField('Email',validators=[DataRequired(),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    confirm_password=PasswordField('Confirm Password', 
-    validators=[DataRequired(), EqualTo('password')])
+    picture=FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
     submit=SubmitField('Update')
 
     def validate_username(self,username):
-        if username.data !=current_user.username:
-            user=User.query.filter_by(username=username.data).first()
+        if username.data!=current_user.username:
+            user=User.query.filter_by(username=username.data).first() 
             if user:
                 raise ValidationError('That username is taken. Choose a different one')
     
     def validate_email(self,email):
-        if email.data !=current_user.email:
+        if email.data!=current_user.email:
             user=User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Choose a different one')
-    
+                raise ValidationError('That email is taken. Please choose a different one')
--- flaskdrop/flaskdrip/routes.py
+++ flaskdrop/flaskdrip/routes.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template,url_for,flash,redirect,request
 from flaskdrip import app,db,bcrypt
 from flaskdrip.models import User,Post 
 from flask_login import login_user,current_user,logout_user,login_required
- 
+import secrets
+import os
+
 posts=[
      {
           'author':'Gaius Gimoro',
@@ -64,10 +66,32 @@ def logout():
      logout_user()
      return redirect(url_for('home'))
 
-@app.route('/account')
+def save_picture(form_picture): #saving picture 
+     random_hex=secrets.token_hex(8)
+     _,f_ext=os.path.splitext(form_picture.filename)
+     picture_fn=random_hex+f_ext #picture file name
+     picture_path=os.path.join(app.root_path,'static/profile_pics',picture_fn)
+     form_picture.save(picture_path)
+
+     return picture_fn
+
+
+@app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
      form=UpdateAccountForm()
+     if form.validate_on_submit():
+          if form.picture.data:
+               picture_file=save_picture(form.picture.data)
+               current_user.image_file=picture_file
+          current_user.username=form.username.data
+          current_user.email=form.email.data
+          db.session.commit()
+          flash('Your account has been updated!','success')
+          return redirect(url_for('account'))
+     elif request.method=='GET':
+          form.username.data=current_user.username
+          form.email.data=current_user.email
      image_file=url_for('static',filename='profile_pics/' + current_user.image_file)
      return render_template('account.html',title='Account',image_file=image_file,
      form=form)
