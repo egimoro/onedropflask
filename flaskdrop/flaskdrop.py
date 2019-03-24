6580b79699e63572e592f29cd342fa36699c973c
egimoro <gimoremma@gmail.com>
2019-03-24 20:25:52 +0100

    create app

--- flaskdrop/flaskdrop.py
+++ flaskdrop/flaskdrop.py
@@ -1,4 +1,6 @@
-from flaskdrip import app
+from flaskdrip import create_app
+
+app=create_app()
 
 if __name__=='__main__': #if the name of the app matches the main script jambo
      app.run(debug=True)  #run the app when debugging
