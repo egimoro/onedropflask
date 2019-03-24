393f0183b6d6a0ac62983affb440ca74c75c5d9c
Emmanuel T. Gimoro <47285000+egimoro@users.noreply.github.com>
2019-03-24 13:44:34 +0100

    update 2
    no merge

--- flaskdrop/flaskdrip/users/utils.py
+++ flaskdrop/flaskdrip/users/utils.py
@@ -20,12 +20,8 @@ def save_picture(form_picture): #saving picture
 
 def send_reset_email(user):
      token=user.get_reset_token()
-<<<<<<< HEAD
-     msg=Message('Password Reset Request',sender='ADMINS',recipients=[user.email])
-=======
-     msg=Message('Password Reset Request',sender='noreply@demo.com',recipients=[user.email])
->>>>>>> 37f9541da7083a9de1322b6987e707ba907c1783
-     msg.body=f'''To reset your password, visit the following link:
+   msg=Message('Password Reset Request',sender='ADMINS',recipients=[user.email])
+   msg.body=f'''To reset your password, visit the following link:
 {url_for('users.reset_token',token=token,_external=True)}
 If you did not make this request then simply ignore this email and no changes will be made
 ''' 
